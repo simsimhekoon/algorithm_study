f59119581a5c987a8207f4e20ae73f635191b977
simsimhekoon <tlfqkfka12@naver.com>
2022-11-02 13:31:05 +0900

    Create C1110.java
    백준 알고리즘 1110번 클리어 코드

---
+++ src/C1110.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class C1110 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String value = "";
+        int valueInt = 0;
+        int value1 = 0;
+        int value2 = 0;
+        int valueSume = 0;
+        int count = 0;
+
+        value = br.readLine();
+        valueInt = Integer.parseInt(value);
+
+        if (valueInt < 10) {
+            valueInt *= 10;
+        }
+
+        int valueNew = valueInt;
+
+        while (true) {
+            count++;
+
+            value1 = valueNew / 10;
+            value2 = valueNew % 10;
+            valueSume = value1 + value2;
+            valueNew = (valueSume % 10) + (value2 * 10);
+
+            if (valueNew == valueInt) {
+                break;
+            }
+        }
+        System.out.println(count);
+    }
+}
